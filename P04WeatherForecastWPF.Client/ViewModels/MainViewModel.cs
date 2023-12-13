7d590cbd2b97fc55828f71c51f7b5bfb9a59a649
kurs <kurs@DESKTOP-V3HDKPQ>
2023-12-13 10:14:10 +0100

    q

--- P04WeatherForecastWPF.Client/ViewModels/MainViewModel.cs
+++ P04WeatherForecastWPF.Client/ViewModels/MainViewModel.cs
@@ -1,25 +1,54 @@
-﻿using System;
+﻿using P04WeatherForecastWPF.Client.Commands;
+using P04WeatherForecastWPF.Client.Models;
+using P04WeatherForecastWPF.Client.Services;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace P04WeatherForecastWPF.Client.ViewModels
 {
     internal class MainViewModel
     {
-        private string cityName = "warszawa"; 
+        private string _cityName = "warszawa";
+        private City[] _cities;
+
+
+        private readonly AccuWeatherService _accuWeatherService;
 
         public string CityName 
         { 
             get 
             {  
-                return cityName; 
+                return _cityName; 
             }
         set 
             {  
-                cityName = value;
+                _cityName = value;
             } 
+        }
+
+        public City[] Cities
+        {
+            get { return _cities; }
+            set { _cities = value; }
+        }
+
+
+        public ICommand LoadCitiesCommand { get; }
+
+        public MainViewModel()
+        {
+            LoadCitiesCommand = new RelayCommand(x => LoadCities());
+            _accuWeatherService = new AccuWeatherService();
+        }
+
+        public async void LoadCities()
+        {
+            Cities = await _accuWeatherService.GetLocations(_cityName); 
         }
     }
 }
