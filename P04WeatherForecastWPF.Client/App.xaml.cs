a1e4f26275178880a4fceb16b4b9240c4e1103a3
kurs <kurs@DESKTOP-V3HDKPQ>
2023-12-13 09:47:54 +0100

    Add project files.

---
+++ P04WeatherForecastWPF.Client/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace P04WeatherForecastWPF.Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ P04WeatherForecastWPF.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04WeatherForecastWPF.Client.ViewModels
+{
+    internal class MainViewModel
+    {
+        private string cityName = "warszawa"; 
+
+        public string CityName { get {  return cityName; } set {  cityName = value; } }
+    }
+}
